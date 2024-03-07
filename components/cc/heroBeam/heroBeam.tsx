01644bb0d7bf7acc972b122a886106f4fbe8afd2
Hurley <hurleywflow@gmail.com>
2024-03-07 17:17:20 +1100

    fix: :art: remove mobile description

--- components/cc/heroBeam/heroBeam.tsx
+++ components/cc/heroBeam/heroBeam.tsx
@@ -32,14 +32,14 @@ function HeroBeam() {
 							className='bg-gradient-to-b from-neutral-400 to-neutral-800 bg-clip-text text-6xl font-bold text-transparent shadow-neutral-500 text-shadow-lg md:text-7xl lg:text-8xl'
 							repeatDelay={5000}
 						/>
-						<div className='my-10 hidden lg:block'>
-						{/* <div className='my-10'> */}
+						{/* <div className='my-10 lg:hidden'>
+							<Description />
+						</div> */}
+						<div className='my-10'>
+							{/* <div className='my-10'> */}
 							<p className='supports-[animation-timeline]:reveal-text text-balance text-xl text-foreground shadow-foreground-400 text-shadow-lg md:text-2xl lg:text-3xl lg:leading-[1]'>
 								{t('hero4')}
 							</p>
-						</div>
-						<div className='my-10 lg:hidden'>
-							<Description />
 						</div>
 					</div>
 				</div>
