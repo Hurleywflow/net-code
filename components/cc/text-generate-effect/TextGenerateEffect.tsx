fb104488ee4c8aa0d9e6b3cfc122cd3db402e8a4
Hurley <hurleywflow@gmail.com>
2024-03-27 23:01:50 +1100

    feat: 9893eb437e47

--- components/cc/text-generate-effect/TextGenerateEffect.tsx
+++ components/cc/text-generate-effect/TextGenerateEffect.tsx
@@ -1,57 +1,59 @@
-// 'use client';
-// import { cn } from '@/lib/utils';
-// import { motion, stagger, useAnimate } from 'framer-motion';
-// import { useEffect } from 'react';
+'use client';
+import { cn } from '@/lib/utils';
+import { motion, stagger, useAnimate } from 'framer-motion';
+import { useEffect } from 'react';
 
-// export const TextGenerateEffect = ({
-// 	words,
-// 	className
-// }: {
-// 	words: string;
-// 	className?: string;
-// }) => {
-// 	const [scope, animate] = useAnimate();
-// 	const wordsArray = words.split(' ');
-// 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-// 	useEffect(() => {
-// 		void animate(
-// 			'span',
-// 			{
-// 				opacity: 1
-// 			},
-// 			{
-// 				duration: 2,
-// 				delay: stagger(0.1)
-// 			}
-// 		);
-// 	}, [scope.current]);
+const TextGenerateEffect = ({
+	words,
+	className
+}: {
+	words: string;
+	className?: string;
+}): JSX.Element => {
+	const [scope, animate] = useAnimate();
+	const wordsArray = words.split(' ');
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		void animate(
+			'span',
+			{
+				opacity: 1
+			},
+			{
+				duration: 2,
+				delay: stagger(0.1)
+			}
+		);
+	}, [animate]);
 
-// 	const renderWords = () => {
-// 		return (
-// 			<motion.div ref={scope}>
-// 				{wordsArray.map((word, idx) => {
-// 					return (
-// 						<motion.span
-// 							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-// 							key={word + idx}
-// 							// changing text format here
-// 							className='text-balance text-xl text-foreground-700 shadow-foreground-400 text-shadow-lg md:text-2xl'
-// 						>
-// 							{word}{' '}
-// 						</motion.span>
-// 					);
-// 				})}
-// 			</motion.div>
-// 		);
-// 	};
+	const renderWords = () => {
+		return (
+			<motion.div ref={scope}>
+				{wordsArray.map((word, idx) => {
+					return (
+						<motion.span
+							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+							key={word + idx}
+							// changing text format here
+							className='text-balance text-xl text-foreground-700 shadow-foreground-400 text-shadow-lg md:text-2xl'
+						>
+							{word}{' '}
+						</motion.span>
+					);
+				})}
+			</motion.div>
+		);
+	};
 
-// 	return (
-// 		<div className={cn('font-normal', className)}>
-// 			<div className='mt-4'>
-// 				<div className=' text-2xl leading-snug tracking-wide text-black dark:text-white'>
-// 					{renderWords()}
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
+	return (
+		<div className={cn('font-normal', className)}>
+			<div className='mt-4'>
+				<div className=' text-2xl leading-snug tracking-wide text-black dark:text-white'>
+					{renderWords()}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TextGenerateEffect;
--- components/cc/text-generate-effect/TextGenerateEffectDemo.tsx
+++ components/cc/text-generate-effect/TextGenerateEffectDemo.tsx
@@ -1,15 +1,15 @@
-// 'use client';
-// import { usePathname } from 'next/navigation';
-// import { TextGenerateEffect } from './TextGenerateEffect';
+'use client';
+import { usePathname } from 'next/navigation';
+import  TextGenerateEffect  from './TextGenerateEffect';
 
 // const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
 // `;
 
-// export function TextGenerateEffectDemo() {
-// 	const pathname = usePathname();
-// 	const words = pathname.includes('/vn')
-// 		? 'Chúng tôi sẽ lên lịch một cuộc gọi hoặc cuộc họp xác định phạm với bạn để thảo luận yêu cầu của bạn và xác định các giải pháp thích hợp, thường mất tới 45 phút. Trong cuộc gọi, chúng tôi sẽ hỏi một số câu hỏi để thu thập thông tin về đối tượng mục tiêu của bạn, bao quát trang web, hướng thiết kế, v.v. Sau cuộc gọi, chúng tôi sẽ chuẩn bị một đề xuất kèm theo báo giá chi tiết. Quá trình này là cần thiết để sản phẩm bàn giao và chi phí trả trước, đảm bảo rằng tất cả chúng ta đều đồng ý, giúp tránh bất kỳ những điều bất ngờ trong quá trình thực hiện dự án.'
-// 		: "We'll schedule a free scoping call or meeting with you to discuss your requirements and determine the appropriate solutions, which usually takes up to 45 minutes. During the call, we'll ask a series of questions to collect information about your intended audience, website vision, competitors, design direction, and more. After the call, we'll prepare a proposal with a detailed quote. The scoping process is essential to ensure that we are all in agreement regarding deliverables and upfront costs, which helps avoid any surprises during the project.";
+export function TextGenerateEffectDemo() {
+	const pathname = usePathname();
+	const words = pathname.includes('/vn')
+		? 'Chúng tôi sẽ lên lịch một cuộc gọi hoặc cuộc họp xác định phạm với bạn để thảo luận yêu cầu của bạn và xác định các giải pháp thích hợp, thường mất tới 45 phút. Trong cuộc gọi, chúng tôi sẽ hỏi một số câu hỏi để thu thập thông tin về đối tượng mục tiêu của bạn, bao quát trang web, hướng thiết kế, v.v. Sau cuộc gọi, chúng tôi sẽ chuẩn bị một đề xuất kèm theo báo giá chi tiết. Quá trình này là cần thiết để sản phẩm bàn giao và chi phí trả trước, đảm bảo rằng tất cả chúng ta đều đồng ý, giúp tránh bất kỳ những điều bất ngờ trong quá trình thực hiện dự án.'
+		: "We'll schedule a free scoping call or meeting with you to discuss your requirements and determine the appropriate solutions, which usually takes up to 45 minutes. During the call, we'll ask a series of questions to collect information about your intended audience, website vision, competitors, design direction, and more. After the call, we'll prepare a proposal with a detailed quote. The scoping process is essential to ensure that we are all in agreement regarding deliverables and upfront costs, which helps avoid any surprises during the project.";
 
-// 	return <TextGenerateEffect words={words} />;
-// }
+	return <TextGenerateEffect words={words} />;
+}
